2e8592a58e0fd4f875b70388fadcd8df186b0c07
Anniballi-Andrea <andreanniballi@gmail.com>
2025-10-28 10:38:14 +0100

    day 2 milestone 1 done

--- assets/js/main.js
+++ assets/js/main.js
@@ -101,6 +101,27 @@ fetch("https://lanciweb.github.io/demo/api/pictures/")
         rowEl.innerHTML = replaceInner
     })
 
+   /* CONSEGNA
+
+Milestone 1
+
+Facciamo in modo di creare un overlay che copra l’intera pagina e all’interno, centrata, disponiamo un’immagine qualunque ed un button di chiusura.
+
+Milestone 2
+
+Facciamo sparire l’overlay con l’aiuto di una classe CSS che imposti il display: none .
+
+Dopodiché facciamo sì che cliccando una qualunque foto. L’overlay ricompaia.
+
+Cliccando invece il button di chiusura, l’overlay scompare nuovamente.
+
+Milestone 3
+
+Inseriamo il pezzo di logica finale: quando una foto viene cliccata, dobbiamo fare in modo che sia proprio quella foto a essere mostrata all’interno dell’overlay.
+
+Ci sono diversi modi di farlo, prova a sperimentare 🙂*/
+
+
 
 
 
